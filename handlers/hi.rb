6b57fe768c46360655681923789891cf96f007c6
Cainã Costa <cainan.costa@gmail.com>
2010-05-02 02:55:29 -0300

    oops, some portuguese things that just slipped out

--- handlers/hi.rb
+++ handlers/hi.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 
 add_handler(/!hi/) do |message|
-  message.send :to => message.channel, :body => "#{message.caller}, olá!"
+  message.send :to => message.channel, :body => "#{message.caller}, hello!"
 end
--- handlers/reload.rb
+++ handlers/reload.rb
@@ -2,5 +2,5 @@
 
 add_handler(/!reload/) do |message|
   reload_handlers
-  message.send :to => message.channel, :body => "#{message.caller}, configuração recarregada com sucesso."
+  message.send :to => message.channel, :body => "#{message.caller}, configuration reloaded successfully."
 end
