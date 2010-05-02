35d409392396d90308b6911b983fa68ce33e31c8
Cainã Costa <cainan.costa@gmail.com>
2010-05-01 23:09:10 -0300

    A little change on ConfigParser class to make it more succinct

--- lib/config_parser.rb
+++ lib/config_parser.rb
@@ -1,11 +1,11 @@
 require 'yaml'
+require 'forwardable'
 
 class ConfigParser
+  extend Forwardable
+  def_delegators :@options, :to_module
+
   def initialize(file = 'config.yml')
     @options = YAML.load_file(file)
-  end
-
-  def options
-    @options
   end
 end
